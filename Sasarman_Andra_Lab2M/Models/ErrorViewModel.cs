8317ed0a1b486758a8d0221e9a67e992918cb274
ANDRA-FLORINA-MARIA SĂSĂRMAN <andra.sasarman@stud.ubbcluj.ro>
2021-10-08 15:15:45 +0300

    Add project files.

---
+++ Sasarman_Andra_Lab2M/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Sasarman_Andra_Lab2M.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
